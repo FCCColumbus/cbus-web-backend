1fff25e37cc8dd3418f46f342af044f142c80dd5
guregu <takosuper1@gmail.com>
2024-09-24 14:17:11 -0400

    Created basic model for ical data based on text file from john riley

--- MeetupUpdateAPI/api/models.py
+++ MeetupUpdateAPI/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django_geopostcodes import GeoPostcodeField
 
-# Create your models here.
+User = get_user_model()
+
+class MyModel(models.Model):
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(max_length=100)
+    summary = models.TextField()
+    description = models.TextField()
+    event_class = models.CharField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #TODO: check the parameters if needed
+    geo = GeoPostcodeField(null=True, blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    url = models.URLField()
+    uuid = models.UUIDField()
